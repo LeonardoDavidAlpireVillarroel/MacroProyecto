6f3e9fd010c3c71c1828a4fb2ef7db94f8949c65
JoseAngelPrada <118129820+JoseAngelPrada@users.noreply.github.com>
2025-05-11 22:55:54 +0200

    Revert "Merge branch 'Alexandra' into Development"
    This reverts commit 280f89aea6eda3262412a23e0fd83682efdfb63f, reversing
    changes made to 17fe00f8da09e9b093c0f4e0162af91a217405b4.

--- MacroProyecto_ProyectosIII/Assets/Scripts/Items/Shops/OpenShop.cs
+++ MacroProyecto_ProyectosIII/Assets/Scripts/Items/Shops/OpenShop.cs
@@ -92,6 +92,7 @@ public class OpenShop : MonoBehaviour
         cg.alpha = 1f;
         cg.interactable = true;
         cg.blocksRaycasts = true;
+        playerController.GetComponent<FruitShoot>().enabled = false;
     }
 
     private void DisableCanvasGroup(CanvasGroup cg)
@@ -99,6 +100,7 @@ public class OpenShop : MonoBehaviour
         cg.alpha = 0f;
         cg.interactable = false;
         cg.blocksRaycasts = false;
+        playerController.GetComponent<FruitShoot>().enabled = true;
         playerController.moveAction.Enable();
     }
 
